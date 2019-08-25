bb658e216b85f4a5aaff2308fa037ab4fbf344a5
kamilzapotoczny <kamil.zapotoczny@pricefx.eu>
2019-08-25 14:14:20 +0200

    Initial commit

---
+++ src/main/java/MyStepdefs.java
@@ -0,0 +1,17 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("^Open the Firefox and launch the application$")
+    public void openTheFirefoxAndLaunchTheApplication() {
+    }
+
+    @When("^Enter the Username and Password$")
+    public void enterTheUsernameAndPassword() {
+    }
+
+    @Then("^Reset the credential$")
+    public void resetTheCredential() {
+    }
+}
